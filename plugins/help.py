e6e45897d6237bf7995be5dcefa8f32a775cf1de
nonepage <chendaxians@outlook.com>
2020-03-09 15:03:00 +0800

    解决多条命令进入时堵塞问题

--- plugins/help.py
+++ plugins/help.py
@@ -5,11 +5,17 @@ import socket
 
 @on_command('help', only_to_me=False)
 async def ipinfo(session: CommandSession):
-    await session.send('NB-Bot v1.0.2\n'
+    await session.send('幽忆云 bot v1.0.2\n'
                        'by 单字汪\n'
-                       'nonepage略作修改\n\n'
+                       '其余贡献者:\n'
+                       'nonepage\n'
+                       '\n\n'
+                        
                        
                        '指令列表:\n'
                        '/ip 查询IP消息\n'
+                       '/gfwtcp 被墙检测\n'
                        '/gfwdns 查询域名DNS信息\n'
-                       '/gfwtcp 被墙检测\n')
+                       '----------------------------\n'
+                       '***欢迎其他开发者加入***\n开源项目地址:https://github.com/result-bit/AirBot'
+                       )
